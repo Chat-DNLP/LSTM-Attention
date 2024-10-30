2d960b5427f567ffa22fffb1384677d4ff8c7581
Susana Suárez <susanasuarezmendoza26@gmail.com>
2024-10-30 11:44:15 +0000

    wandb

--- attention/dot_product_attention.py
+++ attention/dot_product_attention.py
@@ -3,6 +3,4 @@ import torch
 class DotProductAttention:
 
     def compute_score(self, decoder_state, encoder_state):
-        encoder_state_t = encoder_state.transpose(1, 2)
-        product = torch.matmul(decoder_state, encoder_state_t)
-        return product.squeeze()
+        return torch.matmul(decoder_state, encoder_state.transpose(1, 2)).squeeze()
