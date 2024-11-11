2cfe7f1f5581c035dc4ca71783e206fd275fe187
Susana Suárez <susanasuarezmendoza26@gmail.com>
2024-11-11 10:03:11 +0000

    images images_readme

--- attention/bilinear_attention.py
+++ attention/bilinear_attention.py
@@ -18,13 +18,8 @@ class BilinearAttention(nn.Module):
         attention_scores = torch.bmm(encoder_states, hidden_proj)
         attention_weights = attention_scores.squeeze(2)
 
-        # Normalized vectors -> [ 8, 3, 1]
         normalized_vectors = torch.softmax(attention_weights, dim=1).unsqueeze(-1)
-
-        # [ 8, 3, 512] * [ 8, 3, 512] = [8, 3, 512]
         attention_output = normalized_vectors * encoder_states
-
-        # Promedio de los vectores -> [8, 1, 512]
         summed_vectors = torch.sum(attention_output, dim=1, keepdim=True)
 
         return summed_vectors
--- attention/mlp_attention.py
+++ attention/mlp_attention.py
@@ -21,13 +21,8 @@ class MLPAttention(nn.Module):
 
         attention_weights = self.W2(tanh_output).squeeze(2)
 
-        # Normalized vectors -> [ 8, 3, 1]
         normalized_vectors = torch.softmax(attention_weights, dim=1).unsqueeze(-1)
-
-        # [ 8, 3, 512] * [ 8, 3, 512] = [8, 3, 512]
         attention_output = normalized_vectors * encoder_states
-
-        # Promedio de los vectores -> [8, 1, 512]
         summed_vectors = torch.sum(attention_output, dim=1, keepdim=True)
 
         return summed_vectors
